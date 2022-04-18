b1349b0fe5bfb4dd42bec8acc068461cc180a960
lukaszse <llseremak@gmail.com>
2022-04-18 20:46:36 +0200

    update documentation

---
+++ src/test/groovy/org/lukaszse/carRental/service/AvailabilityServiceSpec.groovy
@@ -0,0 +1,37 @@
+package org.lukaszse.carRental.service
+
+import org.lukaszse.carRental.model.TimePeriod
+import org.lukaszse.carRental.repository.ReservationSearchRepository
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.LocalDate
+
+class AvailabilityServiceSpec extends Specification {
+
+    ReservationSearchRepository repository = Mock()
+    AvailabilityService availabilityService = new AvailabilityService(repository)
+
+    @Unroll
+    def "should check if data ranges overlap correctly - test #no"() {
+
+        when: "invoke method to check if data ranges overlaps"
+        def overlap = availabilityService.checkIfPeriodOverlap(firstPeriod, secondPeriod)
+
+        then: "should check correctly if data ranges overlap"
+        overlap == expectedResult
+
+        where:
+        no | firstPeriod           | secondPeriod          | expectedResult
+        1  | getTimePeriod(1, 2)   | getTimePeriod(3, 4)   | false
+        2  | getTimePeriod(2, 4)   | getTimePeriod(3, 5)   | true
+        3  | getTimePeriod(1, 4)   | getTimePeriod(20, 30) | false
+        4  | getTimePeriod(10, 14) | getTimePeriod(7, 13)  | true
+        5  | getTimePeriod(10, 14) | getTimePeriod(7, 60)  | true
+        7  | getTimePeriod(1, 200) | getTimePeriod(7, 60)  | true
+    }
+
+    static def getTimePeriod(final int plusDaysFrom, final int plusDaysTo) {
+        TimePeriod.of(LocalDate.now().plusDays(plusDaysFrom), LocalDate.now().plusDays(plusDaysTo))
+    }
+}
