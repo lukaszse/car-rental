7cb49fa998e7739d708b44fa60cdd34e4612d081
lukaszse <llseremak@gmail.com>
2022-05-22 13:29:33 +0200

    update initial database data

--- src/test/groovy/org/lukaszse/carRental/service/UserServiceSpec.groovy
+++ src/test/groovy/org/lukaszse/carRental/service/UserServiceSpec.groovy
@@ -1,5 +1,6 @@
 package org.lukaszse.carRental.service
 
+import org.lukaszse.carRental.exceptions.WrongPayloadException
 import org.lukaszse.carRental.model.User
 import org.lukaszse.carRental.model.dto.PasswordChangeDto
 import org.lukaszse.carRental.repository.UserRepository
@@ -15,30 +16,55 @@ class UserServiceSpec extends Specification {
     UserRepository userRepository = Mock()
     UserService userService = new UserService(userRepository)
 
-    def "ChangePassword"() {
+    def "should change password"() {
 
-        given: "Preparing all required Mocks"
-        userRepository.findUserByUserName(_ as String) >> Optional.of(new User())
-//        if (newPassword == newPasswordConfirm) {
-//            passwordEncoder.matches(_ as String, _ as String)  >> Boolean.TRUE
-//        } else {
-//            passwordEncoder.matches(_ as String, _ as String)  >> Boolean.FALSE
-//        }
+        given: "Mock all required methods"
+        userRepository.findUserByUserName(_ as String) >> Optional.of(prepareUser())
+        passwordEncoder.matches(_ as String, _ as String) >> true
+        passwordEncoder.encode(_ as String) >> "some password"
+        userService.passwordEncoder = passwordEncoder
 
 
-        and:
+        and: "prepare prepareChangePasswordDto object"
         def passwordChangeDto = prepareChangePasswordDto(oldPassword, newPassword, newPasswordConfirm)
 
-        when:
+        when: "try to change password"
         userService.changePassword(userName, passwordChangeDto)
 
-        then:
+        then: "should throw now exception and invoke changePassword() method once"
         noExceptionThrown()
         1 * userRepository.changePassword(_ as String, _ as String)
 
         where:
         no | userName | oldPassword | newPassword | newPasswordConfirm
-        1  | "joe"    | "password"    | "qwerty"    | "qwerty"
+        1  | "joe"    | "password"  | "qwerty"    | "qwerty"
+        2  | "joe"    | "password"  | "123456"    | "123456"
+    }
+
+    def "should throw exception while trying password with wrong data"() {
+
+        given: "Mock all required methods"
+        userRepository.findUserByUserName(_ as String) >> Optional.of(prepareUser())
+        passwordEncoder.matches(_ as String, _ as String) >> passwordEncoderMatch
+        passwordEncoder.encode(_ as String) >> "some password"
+        userService.passwordEncoder = passwordEncoder
+
+
+        and: "prepare prepareChangePasswordDto object"
+        def passwordChangeDto = prepareChangePasswordDto(oldPassword, newPassword, newPasswordConfirm)
+
+        when: "try to change password"
+        userService.changePassword(userName, passwordChangeDto)
+
+        then: "should throw exception and not change password"
+        thrown(WrongPayloadException.class)
+        0 * userRepository.changePassword(_ as String, _ as String)
+
+        where:
+        no | userName | oldPassword | newPassword | newPasswordConfirm | passwordEncoderMatch
+        1  | "joe"    | "password"  | "qwerty2"   | "qwerty"           | true
+        2  | "joe"    | "password"  | "1234562"   | "123456"           | true
+        3  | "joe"    | "password"  | "123456"    | "123456"           | false
     }
 
     def static prepareChangePasswordDto(String oldPassword, String newPassword, String newPasswordConfirm) {
@@ -49,9 +75,11 @@ class UserServiceSpec extends Specification {
         passwordChangeDto
     }
 
-    def static prepareUser(String userName){
+    def static prepareUser() {
         def user = new User()
         user.setUserName("Joe")
-        user.setPassword('{bcrypt}\$2a\$12$feoSS.Dx/rRdQWfWHeWYZu8txsYcy8Dxt89MWd9U3O8r4CaAKKY3S') // Password = password
+        user.setPassword('{bcrypt}\$2a\$12$feoSS.Dx/rRdQWfWHeWYZu8txsYcy8Dxt89MWd9U3O8r4CaAKKY3S')
+        // Password = password
+        user
     }
 }
